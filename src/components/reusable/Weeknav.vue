9b126cf8f4f8bcb171776ded9539e55ef2dd9109
JPLaura <jlaura@DESKTOP-K0UORQG.localdomain>
2020-11-05 15:34:37 +0200

    commit

--- src/components/reusable/Weeknav.vue
+++ src/components/reusable/Weeknav.vue
@@ -1,16 +1,21 @@
 <template>
-    <div class="bg-white h-16 w-auto border-solid border-4 border-black rounded-lg">
-        <div class="h-full flex items-center float-left"><Arrowbutton2/></div> 
-        <div class="h-full flex items-center float-right"><Arrowbutton/></div>
+    <div class="bg-white h-16 w-auto border-solid border-4 border-black rounded-lg flex flex-row">
+        <div class="h-full flex items-center"><Arrowbutton2/></div>
+        <div class="w-5/10 h-full"></div>
+        <div class="h-full flex items-center"><Arrowbutton/></div>
+        <div class="w-3/10 h-full"></div>
 
     </div>
 </template>
 
 <script>
-import Arrowbutton from './Arrowbutton'
+import Arrowbutton from './Arrowbutton';
+import Arrowbutton2 from './Arrowbutton2';
 export default {
     components: {
-        Arrowbutton
+        Arrowbutton,
+        Arrowbutton2
+
     }
 }
 </script>
